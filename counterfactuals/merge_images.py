d062ed7bd8321e2791adff2f34b692eda12735b7
florenceboettger <florence.boettger@t-online.de>
2023-07-25 20:50:52 +0200

    add query_class functionality to image creation

--- counterfactuals/merge_images.py
+++ counterfactuals/merge_images.py
@@ -13,11 +13,12 @@ from utils.visualize import visualize_edits
 parser = argparse.ArgumentParser(description="Visualize counterfactual explanations by merging images")
 parser.add_argument("--input_path", type=str, required=True)
 parser.add_argument("--seed", type=int, required=False)
-parser.add_argument("--samples", type=int, required=False)
+parser.add_argument("--samples", type=int, required=False, default=10)
 parser.add_argument("--edits", type=int, required=False)
 parser.add_argument("--index", type=int, required=False)
 parser.add_argument("--radius", type=float, required=False)
 parser.add_argument("--type", choices=["any", "identical", "partial", "none"], default="any", required=False)
+parser.add_argument("--query_class", type=int, required=False)
 
 
 
@@ -25,11 +26,12 @@ def main():
     args = parser.parse_args()
 
     dirpath = os.path.join(Path.output_root_dir(), args.input_path)
-    n_samples = args.samples or 10
+    n_samples = args.samples
     radius = args.radius
     n_edits = args.edits
     index = args.index
     match_type = args.type
+    query_class = args.query_class
 
     if args.seed is not None:
         np.random.seed(args.seed)
@@ -45,23 +47,28 @@ def main():
         samples = [index]
     else:
         sample_source = list(counterfactuals.keys())
-        if match_type != "any":
+        if match_type != "any" or query_class is not None:
             sample_source = []
             dict_path = os.path.join(Path.output_root_dir(), "new_results/edits/matches.csv")
             with open(dict_path, "r") as f:
                 reader = list(csv.DictReader(f))
                 for row in reader:
-                    if row["match"] == match_type:
+                    if (row["match"] == match_type or match_type == "any") and (int(row["query_class"]) == query_class or query_class is None):
                         sample_source.append(int(row["query_index"]))
 
-        samples = np.random.choice(sample_source, n_samples)
+        if n_samples == 0:
+            samples = sample_source
+        else:
+            samples = np.random.choice(sample_source, n_samples)
 
     for idx in samples:
-        dirpath_output = os.path.join(Path.output_root_dir(), "examples", args.input_path, f"merge_{idx}")
-        fname = f"output/examples/{args.input_path}/merge_{idx}"
+        path_part = args.input_path
         if match_type != "any":
-            dirpath_output = os.path.join(Path.output_root_dir(), "examples", match_type, args.input_path, f"merge_{idx}")
-            fname = f"output/examples/{match_type}/{args.input_path}/merge_{idx}"
+            path_part = f"{match_type}/{path_part}"
+        if query_class is not None:            
+            path_part = f"{query_class}/{path_part}"
+        dirpath_output = os.path.join(Path.output_root_dir(), "examples", path_part, f"merge_{idx}")
+        fname = f"output/examples/{path_part}/merge_{idx}"
         os.makedirs(dirpath_output, exist_ok=True)
         cf = counterfactuals[idx]
 
